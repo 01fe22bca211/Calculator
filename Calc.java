6339ca2e7fc5c2eff6efce48e8414a3ce9ed3fd0
Vaishali.P <01fe22bca211@kletech.ac.in>
2024-03-05 20:16:04 +0530

    update the calculator code

--- Calc.java
+++ Calc.java
@@ -15,6 +15,14 @@ class Calc{
         return (operand1 / operand2);
     }
 
+    float square(float operand1){
+        return (operand1*operand1);
+    }
+
+    float cube(float operand1){
+        return operand1*(operand1*operand1);
+    }
+
     public static void main(String[] args) {
         float operand1 = 10;
         float operand2 = 10;
@@ -27,5 +35,13 @@ class Calc{
         System.out.println(m_result);
         float d_result = c.division(operand1, operand2);
         System.out.println(d_result);
+
+        float result = c.square(operand1);
+        System.out.println(result);
+
+        float c_result = c.cube(operand1);
+        System.out.println(c_result);
+
+
     }
 }
